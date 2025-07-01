c8cae6bc8b1a82f6029b9a885fbdc4b4706dece8
ankhonk <hongnguyenanh1901@gmail.com>
2025-07-01 22:46:52 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+strings: ["Web Designer.", "Code Developer.","Apps Designer."],
+typeSpeed:50,
+backSpeed:50,
+loop:true,
+});
+
+
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
+
+
+$('.counter').countUp();
+
+AOS.init();
